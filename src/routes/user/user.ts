c7fffe9d1ab98f5b4ec4bec239311090b7026049
Boseok Son <dyacode@proton.me>
2023-08-21 19:05:49 +0900

    Update user.ts

--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -5,6 +5,6 @@ const userController = require('../../api/user/user.controller')
 
 router.post('/login', userController.login)
 router.post('/register', userController.register)
-router.get('/range/:n', userController.range)
+router.get('/getUsers/:n', userController.range)
 
 module.exports = router
